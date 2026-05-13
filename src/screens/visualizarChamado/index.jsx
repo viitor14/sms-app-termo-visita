6fc1c7232acd4f7a55b1d4683b988022bdc6009b
viitor14 <manoelvitor550@gmail.com>
2026-05-13 15:28:23 -0300

    Finalização do layout da tela de visualizar detalhes da visita

--- src/screens/visualizarChamado/index.jsx
+++ src/screens/visualizarChamado/index.jsx
@@ -1,17 +1,28 @@
+import { FontAwesome6 } from "@expo/vector-icons/";
 import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { gerarTermoPDF } from "../../utils/pdfGenerator";
 
-import { neutralColors } from "../../utils/colors";
+import { neutralColors, primaryColors } from "../../utils/colors";
 
 import {
+  Assinatura,
+  BotaoBaixarPDF,
   Container,
   ContainerScroll,
+  DivAssinatura,
   DivData,
   DivDataEHorario,
+  DivDataIcon,
+  DivEquipamentoESerial,
+  DivIconEquipamento,
+  DivMotivoEServico,
+  DivMotivoVisita,
   DivNomeDaUnidade,
   DivVisitaId,
+  MainContainer,
+  TextoSituacao,
   TextoUnidade,
   TituloDiv,
 } from "./styled";
@@ -22,234 +33,300 @@ export default function VisualizarChamado() {
   return (
     <ContainerScroll>
       <Stack.Screen options={{ title: "Detalhes da Visita" }} />
+      <MainContainer>
+        <Container>
+          <DivVisitaId>
+            <TituloDiv>VISITA ID</TituloDiv>
+            <TituloDiv
+              style={{
+                color: "#000",
+              }}
+            >
+              {params.id}
+            </TituloDiv>
+          </DivVisitaId>
+          <DivNomeDaUnidade>
+            <TituloDiv>UNIDADE</TituloDiv>
+            <TextoUnidade>{params.unidade}</TextoUnidade>
+          </DivNomeDaUnidade>
+        </Container>
 
-      <Container>
-        <DivVisitaId>
-          <TituloDiv>VISITA ID</TituloDiv>
-          <TituloDiv
+        <Container>
+          <DivDataEHorario>
+            <DivData>
+              <DivDataIcon>
+                <FontAwesome6
+                  name="calendar"
+                  size={18}
+                  color={neutralColors.neutral}
+                />
+                <Text
+                  style={{
+                    fontFamily: "Poppins_500Medium",
+                    color: neutralColors.neutral,
+                    marginTop: 4,
+                    fontSize: 14,
+                  }}
+                >
+                  Data
+                </Text>
+              </DivDataIcon>
+              <Text style={{ fontFamily: "Poppins_500Medium" }}>
+                {params.data}
+              </Text>
+            </DivData>
+            <DivData>
+              <DivDataIcon>
+                <FontAwesome6
+                  name="clock"
+                  size={18}
+                  color={neutralColors.neutral}
+                />
+                <Text
+                  style={{
+                    fontFamily: "Poppins_500Medium",
+                    color: neutralColors.neutral,
+                    marginTop: 4,
+                  }}
+                >
+                  HORÁRIOS
+                </Text>
+              </DivDataIcon>
+              <Text style={{ fontFamily: "Poppins_500Medium" }}>
+                {params.chegada} - {params.saida}
+              </Text>
+            </DivData>
+          </DivDataEHorario>
+          <DivMotivoEServico>
+            <TituloDiv>Motivo da Visita</TituloDiv>
+            <DivMotivoVisita>
+              <Text style={{ fontFamily: "Poppins_400Regular" }}>
+                {params.motivos}
+              </Text>
+            </DivMotivoVisita>
+          </DivMotivoEServico>
+          <DivMotivoEServico>
+            <TituloDiv>Serviço Realizado</TituloDiv>
+            <DivMotivoVisita>
+              <Text style={{ fontFamily: "Poppins_400Regular" }}>
+                {params.servico}
+              </Text>
+            </DivMotivoVisita>
+          </DivMotivoEServico>
+        </Container>
+
+        <Container style={{ flexDirection: "row", gap: 12 }}>
+          <DivIconEquipamento>
+            <View
+              style={{
+                padding: 12,
+                borderRadius: 50,
+                backgroundColor: primaryColors.p90,
+              }}
+            >
+              <FontAwesome6
+                name="computer"
+                size={24}
+                color={primaryColors.primary}
+              />
+            </View>
+          </DivIconEquipamento>
+          <DivEquipamentoESerial>
+            <View>
+              <Text
+                style={{
+                  fontFamily: "Poppins_500Medium",
+                  color: neutralColors.neutral,
+                  fontSize: 16,
+                }}
+              >
+                Equipamento
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Poppins_500Medium",
+                  textTransform: "uppercase",
+                  marginTop: -6,
+                }}
+              >
+                {params.equipamento}
+              </Text>
+            </View>
+            <View style={{}}>
+              <Text
+                style={{
+                  fontFamily: "Poppins_500Medium",
+                  color: neutralColors.neutral,
+                  fontSize: 16,
+                }}
+              >
+                Nº de Série
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Poppins_500Medium",
+                  textTransform: "uppercase",
+                  marginTop: -6,
+                }}
+              >
+                {params.numeroSerial
+                  ? `${params.numeroSerial}`
+                  : "Não informado"}
+              </Text>
+            </View>
+          </DivEquipamentoESerial>
+        </Container>
+
+        <Container style={{ gap: 14 }}>
+          <View>
+            <TituloDiv>Observações do Técnico</TituloDiv>
+            <DivMotivoVisita>
+              <Text style={{ fontFamily: "Poppins_400Regular" }}>
+                {params.observacao || "Nenhuma observação registrada."}
+              </Text>
+            </DivMotivoVisita>
+          </View>
+          <View>
+            <TituloDiv>Situação Final</TituloDiv>
+            <View style={{ gap: 6 }}>
+              {params.situacao
+                ? params.situacao.split(",").map((item, index) => (
+                    <TextoSituacao status={item.trim()} key={index}>
+                      {item.trim()}
+                    </TextoSituacao>
+                  ))
+                : "Nenhuma situação registrada."}
+            </View>
+          </View>
+        </Container>
+
+        {/* ÁREA DE ASSINATURAS */}
+        <Container style={{ gap: 16 }}>
+          <DivAssinatura>
+            <Text
+              style={{
+                fontFamily: "Poppins_500Medium",
+                color: "#000",
+                fontSize: 16,
+              }}
+            >
+              ASSINATURA DO RESPONSÁVEL
+            </Text>
+            <Assinatura>
+              {params.imgAssinaturaResponsavel && (
+                <Image
+                  source={{ uri: params.imgAssinaturaResponsavel }}
+                  style={{
+                    width: 200,
+                    height: 100,
+                    marginTop: 10,
+                    resizeMode: "contain",
+                  }}
+                />
+              )}
+            </Assinatura>
+
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontFamily: "Poppins_600SemiBold",
+                  fontSize: 18,
+                }}
+              >
+                {params.nomeResponsavel} - {params.cargoResponsavel}
+              </Text>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontFamily: "Poppins_400Regular",
+                  color: neutralColors.neutral,
+                  fontSize: 12,
+                  marginTop: -6,
+                }}
+              >
+                Nome e Cargo/Função
+              </Text>
+            </View>
+          </DivAssinatura>
+          <DivAssinatura>
+            <Text
+              style={{
+                fontFamily: "Poppins_500Medium",
+                color: "#000",
+                fontSize: 16,
+              }}
+            >
+              ASSINATURA DO TÉCNICO
+            </Text>
+            <Assinatura>
+              {params.imgAssinaturaTecnico && (
+                <Image
+                  source={{ uri: params.imgAssinaturaTecnico }}
+                  style={{
+                    width: 200,
+                    height: 100,
+                    marginTop: 10,
+                    resizeMode: "contain",
+                  }}
+                />
+              )}
+            </Assinatura>
+
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontFamily: "Poppins_600SemiBold",
+                  fontSize: 18,
+                }}
+              >
+                {params.nomeTecnico} - {params.matriculaTecnico}
+              </Text>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontFamily: "Poppins_400Regular",
+                  color: neutralColors.neutral,
+                  fontSize: 12,
+                  marginTop: -6,
+                }}
+              >
+                Nome e Matrícula
+              </Text>
+            </View>
+          </DivAssinatura>
+        </Container>
+      </MainContainer>
+      <View
+        style={{
+          backgroundColor: "#fff",
+          padding: 16,
+        }}
+      >
+        <BotaoBaixarPDF
+          onPress={() => {
+            const dadosParaPDF = {
+              ...params,
+              motivos: [params.motivos],
+              situacao: [params.situacao],
+            };
+
+            gerarTermoPDF(dadosParaPDF);
+          }}
+        >
+          <FontAwesome6 name="file-pdf" size={24} color="#fff" />
+          <Text
             style={{
-              color: "#000",
+              color: "#fff",
+              fontFamily: "Poppins_500Medium",
+              marginTop: 4,
             }}
           >
-            {params.id}
-          </TituloDiv>
-        </DivVisitaId>
-        <DivNomeDaUnidade>
-          <TituloDiv>UNIDADE</TituloDiv>
-          <TextoUnidade>{params.unidade}</TextoUnidade>
-        </DivNomeDaUnidade>
-      </Container>
-
-      <Container>
-        <DivDataEHorario>
-          <DivData>
-            <Text
-              style={{
-                fontFamily: "Poppins_500Medium",
-                color: neutralColors.neutral,
-              }}
-            >
-              Data
-            </Text>
-            <Text style={{ fontFamily: "Poppins_500Medium" }}>
-              {params.data}
-            </Text>
-          </DivData>
-          <DivData>
-            <Text
-              style={{
-                fontFamily: "Poppins_500Medium",
-                color: neutralColors.neutral,
-              }}
-            >
-              HORÁRIOS{" "}
-            </Text>
-            <Text style={{ fontFamily: "Poppins_500Medium" }}>
-              {params.chegada} - {params.saida}
-            </Text>
-          </DivData>
-        </DivDataEHorario>
-
-        <Text>{params.motivos}</Text>
-        <Text>{params.servico}</Text>
-      </Container>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          marginTop: 20,
-          padding: 15,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-          Motivo da visita:
-        </Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          marginTop: 10,
-          padding: 15,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-          Serviço realizado:
-        </Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          marginTop: 10,
-          padding: 15,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-          Equipamento / Nº de serie:
-        </Text>
-        <Text>
-          {params.equipamento}
-          {params.numeroSerial && ` / ${params.numeroSerial}`}
-        </Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          padding: 15,
-          marginTop: 10,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-          Observações do Técnico:
-        </Text>
-        <Text style={{ fontSize: 16, fontStyle: "italic", color: "#444" }}>
-          {params.observacao || "Nenhuma observação registrada."}
-        </Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "#fff",
-          padding: 15,
-          marginTop: 10,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-          Situação Final:
-        </Text>
-        <Text style={{ fontSize: 16, fontStyle: "italic", color: "#444" }}>
-          {params.situacao || "Nenhuma situação registrada."}
-        </Text>
-      </View>
-
-      {/* ÁREA DE ASSINATURAS */}
-      <View
-        style={{
-          backgroundColor: "#fff",
-          padding: 15,
-          marginTop: 10,
-          borderRadius: 8,
-        }}
-      >
-        <View style={{ alignItems: "center", marginBottom: 20 }}>
-          <Text style={{ fontWeight: "bold" }}>Assinatura do técnico:</Text>
-          {params.imgAssinaturaTecnico && (
-            <Image
-              source={{ uri: params.imgAssinaturaTecnico }}
-              style={{
-                width: 200,
-                height: 100,
-                marginTop: 10,
-                resizeMode: "contain",
-              }}
-            />
-          )}
-        </View>
-
-        <View
-          style={{
-            alignItems: "center",
-            borderTopWidth: 1,
-            borderColor: "#eee",
-            paddingTop: 15,
-          }}
-        >
-          <Text style={{ fontWeight: "bold", textAlign: "center" }}>
-            Nome e matrícula do técnico:
+            Baixar PDF
           </Text>
-          <Text style={{ textAlign: "center", marginTop: 5 }}>
-            {params.nomeTecnico} - {params.matriculaTecnico}
-          </Text>
-        </View>
+        </BotaoBaixarPDF>
       </View>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          padding: 15,
-          marginTop: 10,
-          borderRadius: 8,
-        }}
-      >
-        <View style={{ alignItems: "center", marginBottom: 20 }}>
-          <Text style={{ fontWeight: "bold" }}>Assinatura do técnico:</Text>
-          {params.imgAssinaturaTecnico && (
-            <Image
-              source={{ uri: params.imgAssinaturaTecnico }}
-              style={{
-                width: 200,
-                height: 100,
-                marginTop: 10,
-                resizeMode: "contain",
-              }}
-            />
-          )}
-        </View>
-
-        <View
-          style={{
-            alignItems: "center",
-            borderTopWidth: 1,
-            borderColor: "#eee",
-            paddingTop: 15,
-          }}
-        >
-          <Text style={{ fontWeight: "bold", textAlign: "center" }}>
-            Nome e cargo do responsável:
-          </Text>
-          <Text style={{ textAlign: "center", marginTop: 5 }}>
-            {params.nomeResponsavel} - {params.cargoResponsavel}
-          </Text>
-        </View>
-      </View>
-
-      {/* Futuramente, aqui podemos colocar um botão "Re-gerar PDF" */}
-      <TouchableOpacity
-        style={{
-          marginTop: 20,
-          backgroundColor: "#007bff",
-          padding: 15,
-          borderRadius: 8,
-          alignItems: "center",
-        }}
-        onPress={() => {
-          const dadosParaPDF = {
-            ...params,
-            motivos: [params.motivos],
-            situacao: [params.situacao],
-          };
-
-          gerarTermoPDF(dadosParaPDF);
-        }}
-      >
-        <Text>Baixar PDF</Text>
-      </TouchableOpacity>
     </ContainerScroll>
   );
 }
--- src/screens/visualizarChamado/styled.js
+++ src/screens/visualizarChamado/styled.js
@@ -1,20 +1,31 @@
 import styled from "styled-components/native";
-import { neutralColors, primaryColors } from "../../utils/colors";
+import {
+  neutralColors,
+  primaryColors,
+  secondaryColors,
+} from "../../utils/colors";
 
 export const ContainerScroll = styled.ScrollView.attrs({
   contentContainerStyle: {
-    padding: 10,
-    paddingBottom: 40,
-    gap: 14,
+    //padding: 20,
+    //paddingBottom: 40,
+    //gap: 14,
   },
 })`
   flex: 1;
-  background-color: ${neutralColors.n90};
+  //background-color: ${neutralColors.n90};
+`;
+
+export const MainContainer = styled.View`
+  flex: 1;
+  padding: 20px;
+  gap: 14px;
+  background-color: ${neutralColors.n95};
 `;
 
 export const Container = styled.View`
   background-color: #fff;
-  padding: 8px 16px;
+  padding: 12px 16px;
   border-radius: 8px;
 `;
 
@@ -51,10 +62,88 @@ export const DivDataEHorario = styled.View`
 `;
 
 export const DivData = styled.View`
-  background-color: ${neutralColors.n90};
+  background-color: ${neutralColors.n95};
   border-radius: 8px;
   flex: 1;
   padding: 12px;
   border: 1px solid ${neutralColors.n70};
   gap: 6px;
+`;
+
+export const DivDataIcon = styled.View`
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+`;
+
+export const DivMotivoEServico = styled.View`
+  margin-top: 14px;
+`;
+
+export const DivMotivoVisita = styled.View`
+  background-color: ${neutralColors.n95};
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid ${neutralColors.n70};
+`;
+
+export const DivEquipamentoESerial = styled.View`
+  align-items: start;
+  gap: 12px;
+`;
+
+export const DivIconEquipamento = styled.View``;
+
+export const DivTituloEquipamento = styled.View`
+  flex-direction: column;
+  background-color: blue;
+`;
+
+export const TextoSituacao = styled.Text`
+  padding: 6px 10px;
+
+  background-color: ${(props) =>
+    props.status === "Problema Resolvido"
+      ? primaryColors.p90
+      : secondaryColors.s95};
+  border: 1px solid;
+
+  border-color: ${(props) =>
+    props.status === "Problema Resolvido"
+      ? primaryColors.p70
+      : secondaryColors.s70};
+
+  border-radius: 20px;
+  font-family: "Poppins_500Medium";
+
+  color: ${(props) =>
+    props.status === "Problema Resolvido"
+      ? primaryColors.primary
+      : secondaryColors.s50};
+  align-self: flex-start;
+`;
+
+export const DivAssinatura = styled.View`
+  border-radius: 8px;
+  border: 1px solid ${neutralColors.n70};
+  padding: 12px;
+  gap: 14px;
+`;
+
+export const Assinatura = styled.View`
+  align-items: center;
+  border: dashed 1px ${neutralColors.n70};
+  border-radius: 8px;
+  background-color: ${neutralColors.n95};
+`;
+
+export const BotaoBaixarPDF = styled.TouchableOpacity`
+  background-color: ${primaryColors.primary};
+  padding: 12px;
+  border-radius: 8px;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
 `;
